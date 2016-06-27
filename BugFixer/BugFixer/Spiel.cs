924a51fb57b043516752d39aa6ba15b38e3d3e5e
tobias.stelzer93@gmx.de <tobias.stelzer93@gmx.de>
2016-06-27 10:13:22 +0200

    dtStatistik

--- BugFixer/BugFixer/Spiel.cs
+++ BugFixer/BugFixer/Spiel.cs
@@ -67,39 +67,14 @@ namespace BugFixer
 
         private void Initialize()
         {
-            OleDbConnectionStringBuilder strBuilder = new OleDbConnectionStringBuilder();
-            strBuilder.Provider = "Microsoft.ACE.OLEDB.12.0";
-            strBuilder.DataSource = Properties.Settings.Default.DbPath;
-
-            con = new OleDbConnection(strBuilder.ConnectionString);
-            con.Open();
-
-            OleDbCommand cmd = con.CreateCommand();
-
             // Programmierer
             dtProgrammierer = new DataTable(); 
             adapterProgrammierer.Fill(dtProgrammierer);
-            /*
-            dtProgrammierer.Columns.Add(new DataColumn("Bezeichnung"));
-            dtProgrammierer.Columns.Add(new DataColumn("Kaufkosten", typeof(int)));
-            dtProgrammierer.Columns.Add(new DataColumn("Verbesserungskosten", typeof(int)));
-            dtProgrammierer.Columns.Add(new DataColumn("Fixwert", typeof(int)));
-            dtProgrammierer.Columns.Add(new DataColumn("Fixwert (Gesamt)", typeof(int)));
-            */
-
             dataGridViewProgrammierer.DataSource = dtProgrammierer;
 
             // Virensucher
             dtVirensucher = new DataTable();
             adapterVirensucher.Fill(dtVirensucher);
-            /*
-            dtVirensucher.Columns.Add(new DataColumn("Bezeichnung"));
-            dtVirensucher.Columns.Add(new DataColumn("Kaufkosten", typeof(int)));
-            dtVirensucher.Columns.Add(new DataColumn("Verbesserungskosten", typeof(int)));
-            dtVirensucher.Columns.Add(new DataColumn("Fixwert", typeof(int)));
-            dtVirensucher.Columns.Add(new DataColumn("Fixwert (Gesamt)", typeof(int)));
-            */
-
             dataGridViewVirensucher.DataSource = dtVirensucher;
 
             // Statistik
@@ -126,41 +101,16 @@ namespace BugFixer
             dt.Rows.Add(new Object[] { "H2", "V2" });
 
             dataGridViewStatistik.DataSource = dt.DefaultView;
-            dataGridViewStatistik.RowHeadersVisible = true;
-            int numColToFreeze = 2;
-            for (int i = 0; i < numColToFreeze - 1; i++)
+            dataGridViewStatistik.RowHeadersVisible = false;
+            dataGridViewStatistik.ColumnHeadersVisible = false;
+            int numColToFreeze = 1;
+            for (int i = 0; i < numColToFreeze; i++)
             {
                 dataGridViewStatistik.Columns[i].Frozen = true;
                 dataGridViewStatistik.Columns[i].DefaultCellStyle = dataGridViewStatistik.RowHeadersDefaultCellStyle;
                 dataGridViewStatistik.Columns[i].ReadOnly = true;
             }
-            /*
-            Dim NumColToFreeze As Int32 = 2
-              With DataGridView1
-                 .RowHeadersVisible = True
-                 For i As Int32 = 0 To NumColToFreeze -1
-                    .Columns(i).Frozen = True ' prevents column from scrolling out of view
-                    .Columns(i).DefaultCellStyle = .RowHeadersDefaultCellStyle ' make it look like a header
-                    .Columns(i).ReadOnly = True
-                 Next
-              End With
-           End Sub
-           */
-
-        }
-
-        private void AddNewRow(DataTable dt)
-        {
-            /*
-            DataRow dr = dt.NewRow();
-            dr[0] = hilfsmittel.Bezeichnung; 
-            dr[1] = hilfsmittel.Kaufkosten; 
-            dr[2] = hilfsmittel.Verbesserungskosten; 
-            dr[3] = hilfsmittel.Fixwert;
-            dr[4] = hilfsmittel.Fixwert;
-
-            dt.Rows.Add(dr);
-            */
+            dataGridViewStatistik.DataSource = dt;
         }
 
         private void Spiel_FormClosing(object sender, FormClosingEventArgs e)
